b87ae0df61654d66b2436e01b6e80f0e21fd3cf2
KevinJordil <kevin.jordil@outlook.com>
2020-01-14 17:59:55 +0100

    Ajout de repetition

--- main.cpp
+++ main.cpp
@@ -30,78 +30,90 @@ int main()
    vector<vector<int>> m1, m2, mResultat;
 
    unsigned numeroOperation;
-   numeroOperation = affichageChoixOperation();
-   switch (numeroOperation){
-      case 1:
-         demandeSaisieVecteur("vecteur 1", v1);
-         demandeSaisieVecteur("vecteur 2", v2);
-         if (addition(v1, v2, vResultat))
+   bool quitter = false;
+
+   while(not quitter){
+
+      numeroOperation = affichageChoixOperation();
+      switch (numeroOperation){
+         case 0:
+            quitter = true;
+            break;
+
+         case 1:
+            demandeSaisieVecteur("vecteur 1", v1);
+            demandeSaisieVecteur("vecteur 2", v2);
+            if (addition(v1, v2, vResultat))
+               afficher(vResultat);
+            else
+               cout << "L'addition des vecteurs n'a pas pu etre effectuee" << endl;
+            break;
+
+         case 2:
+            int scalaire;
+            demandeSaisieVecteur("vecteur", v1);
+            cout << "Saisir le scalaire : ";
+            cin >> scalaire;
+            vResultat = multiplicationParScalaire(scalaire, v1);
             afficher(vResultat);
-         else
-            cout << "L'addition des vecteurs n'a pas pu etre effectuee" << endl;
-         break;
+            break;
 
-      case 2:
-         int scalaire;
-         demandeSaisieVecteur("vecteur", v1);
-         cout << "Saisir le scalaire : ";
-         cin >> scalaire;
-         vResultat = multiplicationParScalaire(scalaire, v1);
-         afficher(vResultat);
-         break;
+         case 3:
+            int resultat;
+            demandeSaisieVecteur("vecteur 1", v1);
+            demandeSaisieVecteur("vecteur 2", v2);
+            if (produitScalaire(v1, v2, resultat))
+               cout << "Le resultat est : " << resultat;
+            else
+               cout << "Le produit scalaire n'a pas pu etre realise" << endl;
+            break;
 
-      case 3:
-         int resultat;
-         demandeSaisieVecteur("vecteur 1", v1);
-         demandeSaisieVecteur("vecteur 2", v2);
-         if (produitScalaire(v1, v2, resultat))
-            cout << "Le resultat est : " << resultat;
-         else
-            cout << "Le produit scalaire n'a pas pu etre realise" << endl;
-         break;
+         case 4:
+            demandeSaisieMatrice("matrice 1", m1);
+            demandeSaisieMatrice("matrice 2", m2);
+            if (addition(m1, m2, mResultat))
+               afficher(mResultat);
+            else
+               cout << "L'addition des matrices n'a pas pu etre effectuee" << endl;
+            break;
 
-      case 4:
-         demandeSaisieMatrice("matrice 1", m1);
-         demandeSaisieMatrice("matrice 2", m2);
-         if (addition(m1, m2, mResultat))
+         case 5:
+            demandeSaisieMatrice("matrice 1", m1);
+            demandeSaisieMatrice("matrice 2", m2);
+            if(produit(m1, m2, mResultat))
+               afficher(mResultat);
+            else
+               cout << "Le produit des matrices n'a pas pu etre effectue" << endl;
+            break;
+
+         case 6:
+            demandeSaisieMatrice("matrice", m1);
+            mResultat = transposee(m1);
             afficher(mResultat);
-         else
-            cout << "L'addition des matrices n'a pas pu etre effectuee" << endl;
-         break;
+            break;
 
-      case 5:
-         demandeSaisieMatrice("matrice 1", m1);
-         demandeSaisieMatrice("matrice 2", m2);
-         if(produit(m1, m2, mResultat))
-            afficher(mResultat);
-         else
-            cout << "Le produit des matrices n'a pas pu etre effectue" << endl;
-         break;
-
-      case 6:
-         demandeSaisieMatrice("matrice", m1);
-         mResultat = transposee(m1);
-         afficher(mResultat);
-         break;
-
-      default:
-         cout << "Saisie erronnee !";
+         default:
+            break;
+      }
    }
+
 
    system("PAUSE");
    return EXIT_SUCCESS;
 }
 
 unsigned affichageChoixOperation(){
+   system("cls");
    cout << "----------------------MENU----------------------" << endl;
+   cout << "0. Quitter" << endl;
    cout << "VECTEURS :" << endl;
-   cout << " 1. Addition de deux vecteurs" << endl;
-   cout << " 2. Multiplication d'un vecteur par un scalaire" << endl;
-   cout << " 3. Produit scalaire de deux vecteurs" << endl;
+   cout << "1. Addition de deux vecteurs" << endl;
+   cout << "2. Multiplication d'un vecteur par un scalaire" << endl;
+   cout << "3. Produit scalaire de deux vecteurs" << endl;
    cout << "MATRICES :" << endl;
-   cout << " 4. Addition de deux matrices" << endl;
-   cout << " 5. Produit de deux matrices" << endl;
-   cout << " 6. Transposee d'une matrices" << endl;
+   cout << "4. Addition de deux matrices" << endl;
+   cout << "5. Produit de deux matrices" << endl;
+   cout << "6. Transposee d'une matrices" << endl;
    cout << "------------------------------------------------" << endl;
 
    cout << "Entrer le numero de votre choix : ";
@@ -109,7 +121,7 @@ unsigned affichageChoixOperation(){
    unsigned choixUtilisateur;
    cin >> choixUtilisateur;
 
-   while(choixUtilisateur < 1 || choixUtilisateur > 5){
+   while(choixUtilisateur > 5){
       cout << "Saisie erronnee !" << endl;
       cout << "Entrer a nouveau le numero de votre choix : ";
       cin >> choixUtilisateur;
