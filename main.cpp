acabd69dc8dee1f104203947339d641407f6e68a
KevinJordil <kevin.jordil@outlook.com>
2020-01-14 11:06:15 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 5
+ Fichier     : main.cpp
+ Auteur(s)   : Kevin Jordil & Filipe Dias Morais
+ Date        : 21.01.2020
+
+ But         :
+
+ Remarque(s) :
+
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   // Interface utilisateur
+
+
+   system("PAUSE");
+   return EXIT_SUCCESS;
+}
