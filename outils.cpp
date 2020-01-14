ce5b75fd1000575826dfb76d674545b976512c1c
KevinJordil <kevin.jordil@outlook.com>
2020-01-14 18:00:05 +0100

    Commentaire

--- outils.cpp
+++ outils.cpp
@@ -20,10 +20,14 @@ using Matrice = std::vector<Vecteur>;
 
 void lire(Vecteur &v)
 {
+   //Ne pas ecrire "Saisir vecteur:"
+   //Saisir la taille du vecteur
+   //Saisir chaque composante
 }
 
 void afficher(const Vecteur &v)
 {
+   //Afficher sans texte
 }
 
 bool addition(const Vecteur &v1, const Vecteur &v2, Vecteur &v)
@@ -40,10 +44,14 @@ bool produitScalaire(const Vecteur &v1, const Vecteur &v2, int &resultat)
 
 void lire(Matrice &m)
 {
+   //Ne pas ecrire "Saisir matrice:"
+   //Saisir la taille de la matrice
+   //Saisir chaque composante
 }
 
 void afficher(const Matrice &m)
 {
+   //Afficher sans texte
 }
 
 bool addition(const Matrice &m1, const Matrice &m2, Matrice &m)
